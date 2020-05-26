f95843fec405eb9fc212449a91973dbf504341eb
Andrew Nyago <nyagoandrew@gmail.com>
2020-05-26 22:38:54 +0300

    Assert that two plus three equals five

--- app/src/test/java/com/janus/a2a1myplatdiarybrandanjones/ExampleUnitTest.kt
+++ app/src/test/java/com/janus/a2a1myplatdiarybrandanjones/ExampleUnitTest.kt
@@ -14,4 +14,9 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun addTwoAndThree_equalsFive(){
+        assertEquals(5,2+3)
+    }
 }
