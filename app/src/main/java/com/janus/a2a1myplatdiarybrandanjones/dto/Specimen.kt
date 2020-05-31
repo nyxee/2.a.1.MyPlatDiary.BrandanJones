180d2c3337c6b447534f1113627ebf5d5b00205d
Andrew Nyago <nyagoandrew@gmail.com>
2020-05-31 23:22:25 +0300

    Use the @exclude annotation to prevent an attribute from being stored in Firebase Cloud Firestore.

--- app/src/main/java/com/janus/a2a1myplatdiarybrandanjones/dto/Specimen.kt
+++ app/src/main/java/com/janus/a2a1myplatdiarybrandanjones/dto/Specimen.kt
@@ -1,9 +1,11 @@
 package com.janus.a2a1myplatdiarybrandanjones.dto
 
+import com.google.firebase.firestore.Exclude
+
 data class Specimen(var plantName: String="", var latitude: String="", var longitude: String="", var description: String="", var datePlanted: String="", var specimenId : String="", var plantId: Int=0) {
     private var _plantEvents: ArrayList<PlantEvent> = ArrayList()
     var plantEvents: ArrayList<PlantEvent>
-        get() = _plantEvents
+        @Exclude get() = _plantEvents
         set(value) { _plantEvents = value}
 
     override fun toString(): String {
