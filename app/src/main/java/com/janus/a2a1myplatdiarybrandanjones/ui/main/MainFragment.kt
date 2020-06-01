8033268466df88c8bda9286901e64456c9b8dadc
Andrew Nyago <nyagoandrew@gmail.com>
2020-06-01 18:31:03 +0300

    show selected specimen info (from the specimen spinner view) on MainFragment.

--- app/src/main/java/com/janus/a2a1myplatdiarybrandanjones/ui/main/MainFragment.kt
+++ app/src/main/java/com/janus/a2a1myplatdiarybrandanjones/ui/main/MainFragment.kt
@@ -14,6 +14,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -96,6 +97,46 @@ class MainFragment : DiaryFragment() {
             (activity as MainActivity).onSwipeLeft()
         }
         prepRequestLocationUpdates()
+
+        spnSpecimens.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            /**
+             * Callback method to be invoked when the selection disappears from this
+             * view. The selection can disappear for instance when touch is activated
+             * or when the adapter becomes empty.
+             *
+             * @param parent The AdapterView that now contains no selected item.
+             */
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            /**
+             *
+             * Callback method to be invoked when an item in this view has been
+             * selected. This callback is invoked only when the newly selected
+             * position is different from the previously selected position or if
+             * there was no selected item.
+             *
+             * Implementers can call getItemAtPosition(position) if they need to access the
+             * data associated with the selected item.
+             *
+             * @param parent The AdapterView where the selection happened
+             * @param view The view within the AdapterView that was clicked
+             * @param position The position of the view in the adapter
+             * @param id The row id of the item that is selected
+             */
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                mSpecimen = parent?.getItemAtPosition(position) as Specimen
+                // use this specimen object to populate our UI fields
+                actvPlantName.setText(mSpecimen.plantName)
+                etDescription.setText(mSpecimen.description)
+                etDatePlanted.setText(mSpecimen.datePlanted)
+                mViewModel.specimen = mSpecimen
+                // trigger an update of the events for this specimen.
+                mViewModel.fetchEvents()
+            }
+        }
+
     }
 
     private fun login() {
